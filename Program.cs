ce04bf689595dcae85437ea270b44bb3860ceda4
SelenaMendia <103127457+SelenaMendia@users.noreply.github.com>
2022-11-14 22:43:43 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Temperatura_28_dias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0, promedio=0;
+            const int DIAS = 7;
+            const int SEM = 4;
+
+            int [ , ] arregloTemp = new int [SEM,DIAS]; 
+
+            for (int i= 0; i < SEM; i++)
+            {
+                for (int j = 0; j < DIAS; j++)
+                {
+                    Console.WriteLine("Ingrese la temperatura para el dia {0} de la semana {1}", j+1, i+1);
+                    arregloTemp [i,j] = Convert.ToInt32(Console.ReadLine());
+
+                acumulador=acumulador+arregloTemp[ i , j ];
+
+                }
+                
+                Console.WriteLine("-- Fin semana {0}", i+1);
+
+            Console.WriteLine("--Fin Semana --");
+            Console.WriteLine("Presione una tecla para continuar...");
+
+            Console.ReadKey();
+
+        }
+        promedio=acumulador/(SEM*DIAS);
+        
+        Console.WriteLine("El promedio mes es: {0} %",promedio);
+    }
+}
+}
+
